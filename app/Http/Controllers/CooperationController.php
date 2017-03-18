0d8d823bce76ebd6c7ffa86fd8c4138ce3240da3
kiaconstruct <costilek@gmail.com>
2017-03-18 18:15:40 +0400

    cooperation v-2

--- app/Http/Controllers/CooperationController.php
+++ app/Http/Controllers/CooperationController.php
@@ -13,10 +13,20 @@ class CooperationController extends Controller
     }
 
     public function ads_store(Request $request) {
+    	$this->validate($request, [
+    		'title' => 'required',
+    		'image' => 'required',
+    		'link' => 'required',
+    		'end' => 'required'
+    	]);
+
     	// record picture in file system
     	$path_to_pictures = base_path().'public/images';
     	$file_name = time() . "_" . Input::file('image')->getClientOriginalName();
-    	dd($file_name);
+    	Input::file('image')->move($path_to_pictures, $file_name);
+
+    	
+    	
     	return 'store';
     }
 
