5395edccf70e84ff9d6aaa0d8f36405476ab11a7
ReymOleg <reymoleg@gmail.com>
2017-03-22 17:34:20 +0300

    cabinet favourites, opened, closed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,18 +46,47 @@ class UserController extends Controller {
 		return json_encode($response);
 	}
 
+	public function get_events_of_user($user_id, $condition, $status) {
+		$query = DB::table('events');
+		$query->leftJoin('event_followers', 'event_followers.event_id', '=', 'events.id');
+		$query->leftJoin('users', 'users.id', '=', 'events.user_id');
+		$query->where('status', $status);
+		$query->select(
+					'events.*',
+					'users.name',
+					'users.gender',
+					'users.age',
+					'users.events_count',
+					'users.followers_count',
+					'users.rating',
+					DB::raw("count(event_followers.event_id) as current_followers")
+				);
+		$query->groupBy('events.id');
+		$query->orderBy('start', 'ASC');
+		$query->where($condition, $user_id);
+		return $query;
+	}
+
 
 // AJAX
 	public function ajax_opened_events() {
-		return view('account/ajax/opened');
+
+		$events_as_guest = $this->get_events_of_user(Auth::user()->id, 'event_followers.follower_id', 1)->get();
+		$events_as_owner = $this->get_events_of_user(Auth::user()->id, 'events.user_id', 1)->get();
+		return view('account/ajax/opened', ['events_as_guest' => $events_as_guest, 'events_as_owner' => $events_as_owner]);
 	}
 
 	public function ajax_closed_events() {
-		return view('account/ajax/closed');
+		$events_as_guest = $this->get_events_of_user(Auth::user()->id, 'event_followers.follower_id', 0)->get();
+		$events_as_owner = $this->get_events_of_user(Auth::user()->id, 'events.user_id', 0)->get();
+		return view('account/ajax/closed', ['events_as_guest' => $events_as_guest, 'events_as_owner' => $events_as_owner]);
 	}
 
 	public function ajax_favourite_events() {
-		return view('account/ajax/favourite');
+		$query = $this->get_events_of_user(Auth::user()->id, 'favorites.user_id', 1);
+		$query->leftJoin('favorites', 'favorites.event_id', '=', 'events.id');
+		$events = $query->get();
+		return view('account/ajax/favourite', ['events' => $events]);
 	}
 
 	public function ajax_create_event() {
--- resources/views/search/ajax/search.blade.php
+++ resources/views/search/ajax/search.blade.php
@@ -3,53 +3,7 @@
 		<h2 class="title">К сожалению, по запросу ничего не найдено :(</h2>
 	@endif
 	@foreach($events as $event)
-	<div class="event">
-
-		<?php $title = (strlen($event->title) > 50) ? substr($event->title,0,50).'...' : $event->title; ?>
-		<div class="event-title"><a href="{{ route('index_event', $event->id) }}" class="title">{{ $title }}</a></div>
-
-		<?php if($event->tags != NULL) { ?>
-			<div class="event-tags">
-				<div class="event-note">Условия входа: </div>
-				<?php
-					$tags_arr = explode(',', $event->tags);
-					foreach ($tags_arr as $key => $tag) {
-						if($tag !== '') { ?>
-							<span class="event-tag">{{ $tag }}</span>
-						<?php } ?>
-					<?php } ?>
-			</div>
-		<?php } ?>
-
-		<?php $description = (strlen($event->description) > 360) ? substr($event->description,0,360).'...' : $event->description; ?>
-		<div class="event-description">{{ $description }}</div>
-
-		<div class="event-inline">
-			<div class="event-place">
-				<div class="event-nm event-note"><i class="fa fa-map-marker" aria-hidden="true"></i> </div>
-				{{ $event->country }}, {{ $event->city }}, {{ $event->place }}
-			</div>
-
-			<div class="event-time">
-				<div class="event-note"><i class="fa fa-clock-o" aria-hidden="true"></i> </div>
-				{{ gmdate("d M H:i, l", $event->start) }}
-			</div>
-
-			<?php
-				$max_peoples = '';
-				if($event->peoples_count !== NULL) {
-					$max_peoples = ' из ' . $event->peoples_count;
-				}
-			?>
-			<div class="event-currentpeoples">
-				<div class="event-note"><i class="fa fa-users" aria-hidden="true"></i> </div>
-				{{ $event->current_followers }} {{ $max_peoples }}
-			</div>
-		</div>
-
-		<div class="event-achivements"></div>
-		<div class="event-rating"></div>
-	</div>
+		@include('templates.event')
 	@endforeach
 
 	<div class="paginate">
