68034c3cf610dce49a0ba7562e602af0c0a453f8
ReymOleg <reymoleg@gmail.com>
2017-03-30 17:29:42 +0300

    repair token

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,6 +5,6 @@
 	<link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
 	<link rel="stylesheet" href="{{ asset('/css/font-awesome/css/font-awesome.min.css') }}">
 	<meta charset="utf-8">
-	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<meta name="csrf-token" content="{{{ csrf_token() }}}">
 </head>
 <body>
